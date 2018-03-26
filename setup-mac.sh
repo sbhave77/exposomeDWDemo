b3d9fc4bf6c8a78717b9a0356e4b8d29f5ca21bd
Shreyas Bhave <shreyas.bhave77@gmail.com>
2018-03-26 02:11:16 -0400

    Add ExposomeDW ipynb demo file and supporting images and setup files.

---
+++ setup-mac.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+sudo easy_install pip
+pip install virtualenv
+virtualenv -p /usr/bin/python2.7 spokes_demo_env
+source spokes_demo_env/bin/activate
+pip install pandas
+pip install psycopg2-binary
+pip install sqlalchemy
+pip install geopandas
+pip install numpy
+pip install scipy
+pip install folium
+pip install matplotlib
+pip install ipython
+pip install jupyter
+
